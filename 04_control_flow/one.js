da59032eafb80d8a6bb47da73473c786949a18e9
Rathod Om <104003389+RathodOm0309@users.noreply.github.com>
2025-03-10 18:48:25 +0530

    Add files via upload

---
+++ 04_control_flow/one.js
@@ -0,0 +1,58 @@
+//  if
+
+/*
+const isUserloggedIn = true
+const temperature = 41
+
+if(temperature) {
+    console.log("Less then 50");
+    
+}
+else{
+console.log("Temperature is greater then 50");
+}
+*/
+
+
+/*
+ const score = 200
+
+if (score > 100) {
+    let power = "fly"
+    console.log(`User power: ${power}`);
+}
+
+console.log(`User power: ${power}`);
+*/
+
+/*
+
+const balance = 1000
+
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
+    
+} else if (balance < 900) {
+    console.log("less than 750");
+    
+} else {
+    console.log("less than 1200");
+
+}
+*/
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard ) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
+
